f9b7320057897f8fea8fd734134dca9388869ab3
JON <jonn.thinkrtc@gmail.com>
2018-08-10 23:51:19 -0400

    added basic intent with onClick function using activity_main.xml

--- app/src/main/java/com/udacity/jonathan/myplaylist/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/myplaylist/MainActivity.java
@@ -1,7 +1,12 @@
 package com.udacity.jonathan.myplaylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+
+    public void openWorkoutPlaylist(View view){
+        Intent i = new Intent(this, WorkoutActivity.class);
+        startActivity(i);
     }
 }
